3e4f69671c046f0fc53b27b5202245d4e57b2d7a
Yuta Shimizu <yuta@soracom.jp>
2020-12-18 17:08:52 +0900

    fix: allow simId and msisdn
    And I avoid an edge case when imei=undefined is put on the top of the
    message

--- tcp/app.rb
+++ tcp/app.rb
@@ -38,7 +38,7 @@ EOS
         sleep 3
         s.close
       end
-    elsif greeting =~ /^im.i=(\d+)/ # IMSIヘッダのみの場合
+    elsif greeting =~ /[A-Za-z]+=(\d+)/ # 署名がない場合
       reply = "Hello Soracom Beam Client! : #{greeting.chomp}\n"
       puts reply
       s.write reply
